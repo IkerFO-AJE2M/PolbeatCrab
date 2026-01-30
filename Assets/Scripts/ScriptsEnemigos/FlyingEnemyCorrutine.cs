fb3feb5efaaf7e71cb52207ed13d08bd2eeabbdd
IkerFO_AJE2M <ikerfo07@educastur.es>
2026-01-30 14:04:46 +0100

    Importado y script
    He importado lo necesario para trabajarlo.

---
+++ Assets/Scripts/ScriptsEnemigos/FlyingEnemyCorrutine.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class FlyingEnemyCorrutine : MonoBehaviour
+{
+
+    public float speedX;
+    public float speedY;
+    [SerializeField] GameObject target;
+    [SerializeField] Collider2D detectionRadious;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlayerDetect()
+    {
+        if (detectionRadious.isTrigger == true) ;
+        Debug.Log("chupame el guebo");
+    }
+}
