5c2e0eb49cc9e77df0e6f85af139b3bde4a9ae96
SrApplin <ikerfo07@educastur.es>
2026-02-01 21:08:28 +0100

    Corrutina rareta
    No funciona del todo bien el animator con la corrutina, meh

--- Assets/Scripts/ScriptsEnemigos/FlyingEnemyCorrutine.cs
+++ Assets/Scripts/ScriptsEnemigos/FlyingEnemyCorrutine.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Net.NetworkInformation;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -18,17 +19,24 @@ public class FlyingEnemyCorrutine : MonoBehaviour
     public float toPlayerDistance;
     public float diveSpeed;
     public float retreveSpeed;
+    private Vector2 movement;
+    private bool _facingRight;
+
+
 
     [SerializeField] GameObject target;
     [SerializeField] Collider2D detectionRadious;
     [SerializeField] Animator flyerAnimation;
+    [SerializeField] Rigidbody2D _flyerRB;
 
     //AnimatorVar
     private bool attacking;
     private bool idle;
     private bool death;
+    private bool isDiving;
     private bool walking;
-    
+    private bool isRetreaving;
+
     void Start()
     {
 
@@ -36,9 +44,16 @@ public class FlyingEnemyCorrutine : MonoBehaviour
 
     void Update()
     {
+        Flip();
+
+        movement = Vector2.zero;
+
         PositionData();
         if (toPlayerDistance < 10f)
         {
+            isDiving = false;
+            isRetreaving = false;
+            walking = true;
             speedX = 3;
             playerPositionY = target.transform.position.y;
             enemyPositionY = transform.position.y;
@@ -48,6 +63,7 @@ public class FlyingEnemyCorrutine : MonoBehaviour
             }
             else
             {
+                walking = false;
                 StartCoroutine(AttackDive());
             }
         }
@@ -56,8 +72,23 @@ public class FlyingEnemyCorrutine : MonoBehaviour
             speedX = 0;
         }
 
+        flyerAnimation.SetBool("Walking", walking);
+
+
     }
-     
+
+    private void Flip()
+    {
+        if (enemyPositionX < playerPositionX && _facingRight == true)
+        {
+            SpriteFlip();
+        }
+        else if (enemyPositionX > playerPositionX && _facingRight == false)
+        {
+            SpriteFlip();
+        }
+    }
+
     IEnumerator AttackDive()
     {
         if (toPlayerDistance < 6f)
@@ -66,15 +97,24 @@ public class FlyingEnemyCorrutine : MonoBehaviour
             PositionData();
             while (toPlayerDistance > 1.65f)
             {
+                attacking = true;
                 Debug.Log("Drivin'");
                 transform.position = Vector2.MoveTowards(transform.position, target.transform.position, diveSpeed * Time.deltaTime);
+                isDiving = true;
+                isRetreaving = false;
+                flyerAnimation.SetBool("Attack", isDiving);
                 yield return null;
             }
 
-            while (toPlayerDistance < 5.9f)
+            attacking = false;
+
+            while (toPlayerDistance < 3f && attacking == false)
             {
                 Debug.Log("Retreavin");
                 transform.position = Vector2.MoveTowards(transform.position, target.transform.position, retreveSpeed * Time.deltaTime * -1f);
+                isRetreaving = true;
+                isDiving = false;
+                flyerAnimation.SetBool("Retreve", isRetreaving);
                 yield return null;
             }
 
@@ -109,5 +149,12 @@ public class FlyingEnemyCorrutine : MonoBehaviour
         enemyPositionY = transform.position.y;
 
         toPlayerDistance = Vector2.Distance(target.transform.position, transform.position);
+    }
+    private void SpriteFlip()
+    {
+        _facingRight = !_facingRight;
+        float localScaleX = transform.localScale.x;
+        localScaleX = localScaleX * -1f;
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
     }
 }
